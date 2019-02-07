12af980198c9cd4d8a6f9d46582042472a8609ee
Robert Seals <raseals@ball.com>
2019-02-07 11:38:27 -0500

    initial checkin

---
+++ src/main/java/com/robertseals/foobar/FoobarApplication.java
@@ -0,0 +1,14 @@
+package com.robertseals.foobar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoobarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoobarApplication.class, args);
+	}
+
+}
+
